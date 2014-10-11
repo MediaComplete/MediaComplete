9cf45686cab19af2e374f8e82446d9e18a606b9d
shankera <rjnshanker@gmail.com>
2014-10-11 17:12:52 -0500

    tried to do some UI stuff

--- MSOE.MediaComplete/MainWindow.xaml.cs
+++ MSOE.MediaComplete/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace MSOE.MediaComplete
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
