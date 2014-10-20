34c8987ac130e4542cb971349d9778a143629141
Brian Kramer <kramerb@msoe.edu>
2014-10-20 18:55:02 -0500

    updated variable declarations and comments

--- MSOE.MediaComplete.Lib/MusicIdentifier.cs
+++ MSOE.MediaComplete.Lib/MusicIdentifier.cs
@@ -15,12 +15,12 @@ namespace MSOE.MediaComplete.Lib
 {
     public class MusicIdentifier
     {
-        private static readonly int FREQ = 22050;
-        private static readonly int SAMPLE_SECONDS = 30;
-        private static readonly int SAMPLE_SIZE = FREQ * SAMPLE_SECONDS;
-        private static readonly string URL = "http://developer.echonest.com";
-        private static readonly string PATH = "/api/v4/song/identify";
-        private static readonly string API_KEY = "MUIGA58IV1VQUOEJ5";
+        private const int Freq = 22050;
+        private const int SampleSeconds = 30;
+        private const int SampleSize = Freq*SampleSeconds;
+        private const string Url = "http://developer.echonest.com";
+        private const string Path = "/api/v4/song/identify";
+        private const string ApiKey = "MUIGA58IV1VQUOEJ5";
 
         public static async Task<string> IdentifySong(string filename) {
             // We have to force "SampleAudio" onto a new thread, otherwise the main thread 
@@ -29,15 +29,14 @@ namespace MSOE.MediaComplete.Lib
             var codegen = new FingerprintGenerator(audioData, 0);
             var code = codegen.GetFingerprintCode().Code;
 
-            var client = new HttpClient();
-            client.BaseAddress = new Uri(URL);
+            var client = new HttpClient {BaseAddress = new Uri(Url)};
             // TODO lookup and add any metadata fields already on the file
-            var response = await client.GetAsync(PATH + "?api_key=" + API_KEY + "&code=" + code); 
+            var response = await client.GetAsync(Path + "?api_key=" + ApiKey + "&code=" + code); 
 
             // Parse the response body.
             var json = JObject.Parse(await response.Content.ReadAsStringAsync());
 
-            // TODO - return useful data
+            // TODO - return useful data and handle failed identification...
             var title = json.SelectToken("response.songs[0].title").ToString();
             return title;
         }
@@ -48,21 +47,21 @@ namespace MSOE.MediaComplete.Lib
         private static float[] SampleAudio(string filename)
         {
             var inFile = filename;
-            List<float> result = new List<float>();
+            var result = new List<float>();
 
             using (var reader = new Mp3FileReader(inFile))
             {
                 using (WaveStream pcmStream = WaveFormatConversionStream.CreatePcmStream(reader))
                 {
-                    var format = new WaveFormat(FREQ, 1);
+                    var format = new WaveFormat(Freq, 1);
                     using (WaveStream readerStream = new WaveFormatConversionStream(format, pcmStream))
                     {
-                        ISampleProvider provider = new Pcm16BitToSampleProvider(readerStream); // Assumes 16 bit... problem?
+                        ISampleProvider provider = new Pcm16BitToSampleProvider(readerStream); // Assumes 16 bit... TODO is this a problem?
                         // Read blocks of samples until no more available
-                        int blockSize = 2000;
-                        float[] buffer = new float[blockSize];
+                        const int blockSize = 2000;
+                        var buffer = new float[blockSize];
                         int rc;
-                        while ((result.Count + blockSize) < SAMPLE_SIZE && (rc = provider.Read(buffer, 0, blockSize)) > 0)
+                        while ((result.Count + blockSize) < SampleSize && (rc = provider.Read(buffer, 0, blockSize)) > 0)
                         {
                             result.AddRange(buffer.Take(rc));
                         }
