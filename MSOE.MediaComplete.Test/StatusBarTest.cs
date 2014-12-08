1a33e1cf9ce6c7f3abad0277cf1b70fbe5d53af5
burnssm <burnssm@msoe.edu>
2014-12-07 18:34:54 -0600

    MC-89 Commit Unit tests

---
+++ MSOE.MediaComplete.Test/StatusBarTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSOE.MediaComplete.Lib;
+
+namespace MSOE.MediaComplete.Test
+{
+    [TestClass]
+    public class StatusBarTest
+    {
+        private string _message = "";
+        private StatusBarHandler.StatusIcon _icon = StatusBarHandler.StatusIcon.None;
+
+        [TestMethod]
+        public void RaiseStatusBarEvent_StatusMessage_ValidStatusMessage()
+        {
+            const string testMessage = "Test is successful";
+            const StatusBarHandler.StatusIcon testIcon = StatusBarHandler.StatusIcon.Success;
+            
+            StatusBarHandler.RaiseStatusBarEvent += HandleStatusBarChangeEvent;
+            StatusBarHandler.ChangeStatusBarMessage(testMessage, testIcon);
+
+            Assert.AreEqual(testMessage, _message);
+            Assert.AreEqual(testIcon, _icon);
+        }
+
+        private void HandleStatusBarChangeEvent(string status, StatusBarHandler.StatusIcon statusIcon)
+        {
+            _message = status;
+            _icon = statusIcon;
+
+        }
+    }
+}
