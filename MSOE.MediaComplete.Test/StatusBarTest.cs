cf7161986ac8a99619d3e7b553a3f37a70a0604c
burnssm <burnssm@msoe.edu>
2014-12-10 16:22:12 -0600

    MC-88 Fixed the test to work

--- MSOE.MediaComplete.Test/StatusBarTest.cs
+++ MSOE.MediaComplete.Test/StatusBarTest.cs
@@ -8,26 +8,36 @@ namespace MSOE.MediaComplete.Test
     {
         private string _message = "";
         private StatusBarHandler.StatusIcon _icon = StatusBarHandler.StatusIcon.None;
+        private int _count;
 
         [TestMethod]
         public void RaiseStatusBarEvent_StatusMessage_ValidStatusMessage()
         {
             const string testMessage = "Test is successful";
             const StatusBarHandler.StatusIcon testIcon = StatusBarHandler.StatusIcon.Success;
-            StatusBarHandler.Instance.Interval = 0;
+            const string clearMessage = "";
+            const StatusBarHandler.StatusIcon clearIcon = StatusBarHandler.StatusIcon.None;
+            StatusBarHandler.Instance.Interval = .1;
             
             StatusBarHandler.Instance.RaiseStatusBarEvent += HandleStatusBarChangeEvent;
             StatusBarHandler.Instance.ChangeStatusBarMessage(testMessage, testIcon);
 
             Assert.AreEqual(testMessage, _message);
             Assert.AreEqual(testIcon, _icon);
+
+            while (_count < 2)
+            {
+            }
+
+            Assert.AreEqual(clearMessage, _message);
+            Assert.AreEqual(clearIcon, _icon);
         }
 
         private void HandleStatusBarChangeEvent(string status, StatusBarHandler.StatusIcon statusIcon)
         {
             _message = status;
             _icon = statusIcon;
-
+            _count++;
         }
     }
 }
