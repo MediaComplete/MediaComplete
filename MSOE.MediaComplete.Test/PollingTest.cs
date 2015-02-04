65e938db96703f4fd3ae1f2b5d25182b334a71af
shankera <rjnshanker@gmail.com>
2015-02-03 23:26:16 -0600

    removed unused import

--- MSOE.MediaComplete.Test/PollingTest.cs
+++ MSOE.MediaComplete.Test/PollingTest.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Runtime.InteropServices;
+﻿using System.IO;
 using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MSOE.MediaComplete.Lib;
