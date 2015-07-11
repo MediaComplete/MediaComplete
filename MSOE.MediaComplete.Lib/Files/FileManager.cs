8e9f01ab064a5fc89d021f18bca06f60b70e5bc0
shankera <rjnshanker@gmail.com>
2015-07-10 20:06:10 -0400

    making changes, not finished

--- MSOE.MediaComplete.Lib/Files/FileManager.cs
+++ MSOE.MediaComplete.Lib/Files/FileManager.cs
@@ -72,53 +72,6 @@ namespace MSOE.MediaComplete.Lib.Files
         }
 
         #region File Operations
-        /// <summary>
-        /// Copies a file between two specified paths. 
-        /// This is currently only used in the Importer
-        /// </summary>
-        /// <param name="file">Original location of the song, including filename</param>
-        /// <param name="newFile">Destination location of the song, including filename</param>
-        public void CopyFile(SongPath file, SongPath newFile)
-        {
-            File.Copy(file.FullPath, newFile.FullPath);
-        }
-
-        /// <summary>
-        /// Create a folder at a specified location.
-        /// Used by Sorter and to initialize music/playlist folders where necessary
-        /// </summary>
-        /// <param name="directory">Destination location to create the folder, including foldername</param>
-        public void CreateDirectory(DirectoryPath directory)
-        {
-            Directory.CreateDirectory(directory.FullPath);
-        }
-
-        /// <summary>
-        /// Verifies if the specified directory exists.
-        /// </summary>
-        /// <param name="directory">directory location to check</param>
-        /// <returns>true if the directory exists</returns>
-        /// <returns>false if the directory does not exist</returns>
-        public bool DirectoryExists(DirectoryPath directory)
-        {
-            return Directory.Exists(directory.FullPath);
-        }
-
-        // TODO MC-35 keep directories and files that aren't music, so they can be managed in-app
-        /// <summary>
-        /// Verifies if the specified directory has no child directories or music files.
-        /// 
-        /// For now, we don't care about non-music files.
-        /// </summary>
-        /// <param name="directory">directory location to check</param>
-        /// <returns>true if the directory is empty</returns>
-        /// <returns>false if the directory contains additional directories or files</returns>
-        public bool DirectoryEmpty(DirectoryPath directory)
-        {
-            var hasDirs = Directory.EnumerateDirectories(directory.FullPath).Any();
-            var hasMusic = new DirectoryInfo(directory.FullPath).EnumerateFiles().GetMusicFiles().Any();
-            return hasDirs || hasMusic;
-        }
 
         /// <summary>
         /// Moves a file from an old location to a new one
@@ -132,45 +85,11 @@ namespace MSOE.MediaComplete.Lib.Files
         {
             var sourceDir = oldFile.SongPath.Directory;
             if (!_cachedFiles.ContainsKey(oldFile.Id)) throw new ArgumentException();
-            
-            _cachedFiles[oldFile.Id].MoveTo(newFile.FullPath);
+            FileSystem.instance.MoveFile(_cachedFiles[oldFile.Id], newFile);
             ScrubEmptyDirectories(sourceDir);
         }
 
-        /// <summary>
-        /// Verifies if the specified file exists.
-        /// </summary>
-        /// <param name="file">file location to check</param>
-        /// <returns>true if the file exists</returns>
-        /// <returns>false if the file does not exist</returns>
-        public bool FileExists(SongPath file)
-        {
-            return File.Exists(file.FullPath);
-        }
-
-        /// <summary>
-        /// used to migrate an entire directories files and folders to a new location.
-        /// </summary>
-        /// <param name="oldPath">Original directory to move</param>
-        /// <param name="newPath">Destination to move directory to</param>
-        public void MoveDirectory(DirectoryPath oldPath, DirectoryPath newPath)
-        {
-            if (Directory.Exists(newPath.FullPath)) throw new IOException("Destination directory already exists");
-            var sourceDir = new DirectoryInfo(oldPath.FullPath);
-
-            var folders = sourceDir.GetDirectories().ToList();
-            var files = sourceDir.GetFiles().ToList();
-
-            if (!Directory.Exists(newPath.FullPath))
-                Directory.CreateDirectory(newPath.FullPath);
-
-            folders.ForEach(x => x.MoveTo(newPath.FullPath + Path.DirectorySeparatorChar + x.Name));
-            files.ForEach(x => x.MoveTo(newPath.FullPath + Path.DirectorySeparatorChar + x.Name));
-
-            ScrubEmptyDirectories(sourceDir);
-        }
-
-
+        
         /// <summary>
         /// Moves a file from the directory of songPath to the directory at newFile. 
         /// This is used in the importer, to move a file that does not exist in our directory into the working directory.
---
+++ MSOE.MediaComplete.Lib/NinjectBindings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MSOE.MediaComplete.Lib.Files;
+
+namespace MSOE.MediaComplete.Lib
+{
+    public class NinjectBindings:Ninject.Modules.NinjectModule 
+    {
+        public override void Load()
+        {
+            Bind<IFileManager>().To<FileManager>().InSingletonScope();
+        }
+    }
+}
