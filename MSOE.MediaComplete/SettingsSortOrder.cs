3a23a1cc619f93641e23edf85c9b841e9558581e
shankera <rjnshanker@gmail.com>
2015-10-18 00:32:39 -0400

    forgot to save files

--- MSOE.MediaComplete/SettingsSortOrder.cs
+++ MSOE.MediaComplete/SettingsSortOrder.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Shapes;
 using MSOE.MediaComplete.Lib.Metadata;
 using MSOE.MediaComplete.Lib.Sorting;
 using Button = System.Windows.Controls.Button;
